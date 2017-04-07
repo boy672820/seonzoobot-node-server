9538b4100143fb6461dd2ba16b20e91746691f3b
boy672820 <boy672820@gmail.com>
2017-04-07 11:49:10 +0900

    es6 sync

--- server/app.js
+++ server/app.js
@@ -1,15 +1,17 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+'use strict';
 
-var index = require('./routes/index');
-var chatting = require('./routes/chatting');
-var webhook = require('./routes/webhook');
+import express from 'express';
+import path from 'path';
+import favicon from 'serve-favicon';
+import logger from 'morgan';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
 
-var app = express();
+import index from './routes/index';
+import chatting from './routes/chatting';
+import webhook from './routes/webhook';
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, '/../views'));
@@ -28,21 +30,21 @@ app.use( '/chatting', chatting );
 app.use( '/webhook', webhook );
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+app.use( ( req, res, next ) => {
+  var err = new Error( 'Not Found' );
   err.status = 404;
-  next(err);
-});
+  next( err );
+} );
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use( (err, req, res, next ) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get( 'env' ) === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  res.status( err.status || 500 );
+  res.render( 'error' );
+} );
 
 module.exports = app;
